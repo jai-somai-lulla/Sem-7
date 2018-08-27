f4ca3dcd6d920eeb51bdf795b57fe358f0eefccc
jai-the-bear <jaithebear@gmail.com>
2018-08-27 10:03:03 +0530

    Start Chat-Bot

---
+++ LP-AI/Eclipse/ChatBot/src/cb.java
@@ -0,0 +1,9 @@
+import org.alicebot.ab.*; 
+public class cb {
+	public static void main(String args[]) {
+		 String botname="mybot"; Bot bot = new Bot(botname); 
+		//System.out.println("Hello");
+		// bot.brain.nodeStats();
+         String textLine = "";
+	}
+}
