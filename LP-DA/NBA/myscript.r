e7665fd013a6d18710f019e48ec55ac63da5db56
jai-the-bear <jaithebear@gmail.com>
2018-09-03 10:20:29 +0530

    Naive Bayes and dataset for cars

--- LP-DA/NBA/myscript.r
+++ LP-DA/NBA/myscript.r
@@ -4,11 +4,11 @@ library(e1071)
 dat = read.csv("mydata.csv", header = TRUE)
 head(dat)
 #names(dat)
-summary(dat)
+#summary(dat)
 
-#smp_size = floor(0.75*nrow(dat))
+#smp_size = floor(0.25*nrow(dat))
 #print("Test Size smp_size")
-
+#print(smp_size)
 
 sample = sample.split(dat,SplitRatio = 0.75) # splits the data in the ratio mentioned in SplitRatio. After splitting marks these rows as logical TRUE and the the remaining are marked as logical FALSE
 train1 =subset(dat,sample ==TRUE) # creates a training dataset named train1 with rows which are marked as TRUE
